dddb0a9fec401d9fa1531d613749cbdb3718eeda
p22cgussen <p22cgussen@fairfieldprep.org>
2021-09-23 11:06:38 -0400

    edit hw2-3

--- hw2-3-1.py
+++ hw2-3-1.py
@@ -1,3 +1,5 @@
+# author: CCG 9/21/21
+
 print("PPPPP   Y   Y   TTTTT   H   H   OOOOO   N   N")
 print("P   P    Y Y      T     H   H   O   O   NN  N")
 print("PPPPP     Y       T     HHHHH   O   O   N N N")
